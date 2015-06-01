5d6ef862f1dec0d5f7250e48babc822e31ed3fea
weswhitney <wesleywhitney@gmail.com>
2015-05-31 19:54:26 -0600

    added an about controller and a button the about template

---
+++ app/controllers/about.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    showRealName: function () {
+      alert('Dracula was some dude back in the day');
+    }
+  }
+});
