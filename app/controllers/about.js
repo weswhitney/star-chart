fc8f157c61aab5135ce1b0d28707d27ac7bd4447
weswhitney <wesleywhitney@gmail.com>
2015-05-31 20:33:36 -0600

    added logic to about template and actions to the about controller

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isPictureShowing: false,
   actions: {
     showRealName: function () {
       alert('Dracula was some dude back in the day');
+    },
+    showPicture: function () {
+      this.set('isPictureShowing', true);
+    },
+    hidePicture: function () {
+      this.set('isPictureShowing', false);
     }
   }
 });
